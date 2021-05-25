9ae1450518cb1037195812c3e3c6e1c6f6683392
AlecDr <alecsander_rs@hotmail.com>
2021-05-25 20:36:12 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'package:background_fetch/background_fetch.dart';
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:collection/collection.dart';
+
+// [Android-only] This "Headless Task" is run when the Android app
+// is terminated with enableHeadless: true
+void backgroundFetchHeadlessTask(HeadlessTask task) async {
+  String taskId = task.taskId;
+  bool isTimeout = task.timeout;
+  if (isTimeout) {
+    // This task has exceeded its allowed running-time.
+    // You must stop what you're doing and immediately .finish(taskId)
+    print("[BackgroundFetch] Headless task timed-out: $taskId");
+    BackgroundFetch.finish(taskId);
+    return;
+  }
+  print('[BackgroundFetch] Headless event received.');
+  // Do your work here...
+  // Get a location using getDatabasesPath
+  var databasesPath = await getDatabasesPath();
+  String path = join(databasesPath, 'demo.db');
+
+  // Delete the database
+  await deleteDatabase(path);
+
+  // open the database
+  Database database = await openDatabase(path, version: 1,
+      onCreate: (Database db, int version) async {
+    // When creating the db, create the table
+    await db.execute(
+        'CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
+  });
+
+  // Insert some records in a transaction
+  await database.transaction((txn) async {
+    int id1 = await txn.rawInsert(
+        'INSERT INTO Test(name, value, num) VALUES("some name", 1234, 456.789)');
+    print('inserted1: $id1');
+    int id2 = await txn.rawInsert(
+        'INSERT INTO Test(name, value, num) VALUES(?, ?, ?)',
+        ['another name', 12345678, 3.1416]);
+    print('inserted2: $id2');
+  });
+
+  // Update some record
+  int count = await database.rawUpdate(
+      'UPDATE Test SET name = ?, value = ? WHERE name = ?',
+      ['updated name', '9876', 'some name']);
+  print('updated: $count');
+
+  // Get the records
+  List<Map> list = await database.rawQuery('SELECT * FROM Test');
+  List<Map> expectedList = [
+    {'name': 'updated name', 'id': 1, 'value': 9876, 'num': 456.789},
+    {'name': 'another name', 'id': 2, 'value': 12345678, 'num': 3.1416}
+  ];
+  print(list);
+  print(expectedList);
+  assert(const DeepCollectionEquality().equals(list, expectedList));
+
+  // Count the records
+  count = Sqflite.firstIntValue(
+      await database.rawQuery('SELECT COUNT(*) FROM Test'));
+  assert(count == 2);
+
+  // Delete a record
+  count = await database
+      .rawDelete('DELETE FROM Test WHERE name = ?', ['another name']);
+  assert(count == 1);
+
+  // Close the database
+  await database.close();
+
+  BackgroundFetch.finish(taskId);
+}
+
+void main() {
+  runApp(MyApp());
+
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        //
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  Future<void> initPlatformState() async {
+    // Configure BackgroundFetch.
+    int status = await BackgroundFetch.configure(
+        BackgroundFetchConfig(
+            minimumFetchInterval: 15,
+            stopOnTerminate: false,
+            enableHeadless: true,
+            requiresBatteryNotLow: false,
+            requiresCharging: false,
+            requiresStorageNotLow: false,
+            requiresDeviceIdle: false,
+            requiredNetworkType: NetworkType.NONE), (String taskId) async {
+      // <-- Event handler
+      // This is the fetch-event callback.
+      print("[BackgroundFetch] Event received $taskId");
+
+      // IMPORTANT:  You must signal completion of your task or the OS can punish your app
+      // for taking too long in the background.
+      BackgroundFetch.finish(taskId);
+    }, (String taskId) async {
+      // <-- Task timeout handler.
+      // This task has exceeded its allowed running-time.  You must stop what you're doing and immediately .finish(taskId)
+      print("[BackgroundFetch] TASK TIMEOUT taskId: $taskId");
+      BackgroundFetch.finish(taskId);
+    });
+    print('[BackgroundFetch] configure success: $status');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //
+    initPlatformState();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          //
+          //
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
